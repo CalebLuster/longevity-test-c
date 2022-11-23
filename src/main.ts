50059494046017dc7cb24dcd6580d7c01b3b5c26
Caleb Luster <cwl117@outlook.com>
2022-11-23 10:27:32 -0600

    added .

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import VueQrcode from '@chenfengyuan/vue-qrcode';
-
-createApp(App)
-.component("qrcode", VueQrcode)
-.mount('#app')
